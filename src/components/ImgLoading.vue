dd72b0600aa63efee3d3e9500d379ddeafbdfe30
mr_zyp <694534942@qq.com>
2018-04-20 00:12:01 +0800

    pic loading style fix

--- src/components/ImgLoading.vue
+++ src/components/ImgLoading.vue
@@ -1,23 +1,26 @@
 <template>
-
-    <img :src="url">
+    
+    <img :src="url" v-if="isShowImg"/> 
+    <i v-else class="fa fa-spinner fa-pulse fa-3x fa-fw margin-bottom"></i>
 </template>
 <script>
     export default {
         props: ['src'], // 父组件传过来所需的url
         data() {
             return {
-                url: "../static/Img/FindMusic/loading.gif"// 先加载loading.gif
+                url: "../static/Img/FindMusic/loading.gif",// 先加载loading.gif
+                isShowImg:false
             }
         },
         mounted() {
             var newImg = new Image()
             newImg.src = this.src
             newImg.onerror = () => {    // 图片加载错误时的替换图片
-                newImg.src = './static/Img/FindMusic/imgerror.jpeg'
+                newImg.src = './static/Img/FindMusic/imgerror.gif'
             }
             newImg.onload = () => { // 图片加载成功后把地址给原来的img
                 this.url = newImg.src
+                this.isShowImg=true
             }
         }
     }
